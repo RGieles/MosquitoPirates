9a6e652fb0dccec03f5c596d557b278937ab1ac8
Kevin <103201670+GrandMasterKevin@users.noreply.github.com>
2024-07-09 13:40:36 +0200

    Code INIT
    Setup code mechanics

---
+++ Mosquito Pirates/Assets/Scripts/BodyPart.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour
+{
+    public int multiplier;
+
+    public int GetMultiplier()
+    {
+        return multiplier;
+    }
+}
