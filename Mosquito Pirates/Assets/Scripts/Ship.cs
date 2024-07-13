38d6816aec4cb1c6acfcc6b3f886bd9c6916093b
RGieles <rhonja@gmail.com>
2024-07-13 17:33:13 +0200

    Tweaking

--- Mosquito Pirates/Assets/Scripts/Ship.cs
+++ Mosquito Pirates/Assets/Scripts/Ship.cs
@@ -10,15 +10,10 @@ public class Ship : MonoBehaviour
 
     public TMP_Text scoreLabel;
 
-    float currentSpeed;
     public float baseSpeed;
-    public float accel;
-
     public float baseTurnSpeed;
-    float currentTurnSpeed;
-    public float decel;
 
-    public GameObject endScreen; 
+    public GameObject endScreen;
 
     public bool stopMovement = false;
     Rigidbody thisBody;
@@ -26,66 +21,36 @@ public class Ship : MonoBehaviour
     public AudioSource deathSound;
     public AudioSource pickupSound;
 
-
     public float vertSpeed;
     public float horSpeed;
     public float forwardSpeed;
 
-
     void Start()
     {
-        thisBody =  GetComponent<Rigidbody>();
-    }
-
-    void FixedUpdate()
-    {
-        accel = 1 * Time.time;
-        decel = 1 * Time.time;
+        thisBody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if(!stopMovement)
+        if (!stopMovement)
         {
-            //currentSpeed = baseSpeed * accel;
-            //currentTurnSpeed = baseTurnSpeed - decel;
+            Vector3 movement = Vector3.zero;
+            movement += transform.right * Input.GetAxis("Horizontal") * horSpeed * Time.deltaTime;
+            movement += Vector3.up * Input.GetAxis("Vertical") * vertSpeed * Time.deltaTime;
+            movement += transform.forward * forwardSpeed * Time.deltaTime;
 
-            // thisBody.AddForce(transform.forward * currentSpeed);
-
-            //if(Input.GetKey(KeyCode.A))
-            //{
-            //    thisBody.AddForce(transform.right * currentTurnSpeed);
-            //}
-
-            //if(Input.GetKey(KeyCode.D))
-            //{
-            //   thisBody.AddForce(transform.right * -currentTurnSpeed);
-            //}
-
-            //if(Input.GetKey(KeyCode.W))
-            //{
-            //   thisBody.AddForce(transform.up * -currentTurnSpeed);
-            //}
-
-            //if(Input.GetKey(KeyCode.S))
-            //{
-            //    thisBody.AddForce(transform.up * currentTurnSpeed);
-            //}
-            thisBody.velocity = new Vector3();
-            thisBody.velocity += transform.right * Input.GetAxis("Horizontal") * horSpeed * Time.deltaTime;
-            thisBody.velocity += Vector3.up * Input.GetAxis("Vertical") * vertSpeed * Time.deltaTime;
-            thisBody.velocity += transform.forward * forwardSpeed * Time.deltaTime;
+            thisBody.velocity = movement;
         }
     }
 
     void OnCollisionEnter(Collision coll)
     {
         //Ew, tags
-        if(coll.gameObject.tag == "BodyPart")
+        if (coll.gameObject.tag == "BodyPart")
         {
             deathSound.Play();
             stopMovement = true;
-            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            thisBody.velocity = Vector3.zero;
 
             int multiplier = coll.gameObject.GetComponent<BodyPart>().GetMultiplier();
             score *= multiplier;
